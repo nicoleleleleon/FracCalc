711307f028ca40f3551e4aa0c6ed5e96180e1547
APCS2nd <APCS2nd@MHS-Dreyer-15>
2018-11-20 09:51:31 -0800

    checkpt1 done

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -5,24 +5,20 @@ public class FracCalc {
 
     public static void main(String[] args) 
     {
+    	parts("3_1/2");
         // TODO: Read the input from the user and call produceAnswer with an equation
     	System.out.println("What is the equation you'd like me to compute?");
     	Scanner userInput = new Scanner(System.in);
-    	boolean done = false;
-    	String response = "";
-    	String input = "";
-    	while (!done) {
-			input = userInput.nextLine(); //gets equation
-			produceAnswer(input);  //puts into method
-			System.out.println("Do you want to keep going? Type \"quit\" to end. Otherwise, type anything!");
-			response = userInput.nextLine();//they'll type something
-			if (response.equals("quit")) { //if it is "quit"...
-				done = true;
-				System.out.println("Have a nice day!");
-			}else {
-				System.out.println("Give me another equation!");
-			}
+    	String input = userInput.nextLine(); //gets equation
+    	while (!input.equals("quit")) {
+			System.out.println(produceAnswer(input));  //puts into method
+			System.out.println("Do you want to keep going? Type \"quit\" to end.");
+			input = userInput.nextLine();
+	
     	}
+			
+		
+		
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -38,17 +34,29 @@ public class FracCalc {
         // TODO: Implement this function to produce the solution to the input
    //    String array = Arrays.toString(input.split(" "));
    //    System.out.println(array);
-       String [] equat = input.split(" ");
+       String[] equat = input.split(" ");
        String operand1 = equat[0];
        String operator = equat[1];
        String operand2 = equat[2];
 //       int num1 = Integer.parseInt(operand1);
  //      int num2 = Integer.parseInt(operand2);
-      System.out.println(operand2);
       
-        return "";
+        return operand2;
     }
-
+	    
+   public static String parts(String input) {
+    	if(input.indexOf("_")>0 && input.indexOf("/")>0) {
+    		String[] splitUnderscore = input.split("_");
+    		String[] splitSlash =  Arrays.toString(splitUnderscore).split("/");
+   		System.out.println(Arrays.toString(splitSlash));
+   		//does it know the elements? test--> at 0,1,2
+    	return Arrays.toString(splitSlash);
+    	}else { 
+    		
+    		return input;
+   
+   }
     // TODO: Fill in the space below with any helper methods that you think you will need
-    
+}
+
 }
