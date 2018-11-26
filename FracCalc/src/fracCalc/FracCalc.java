68f006ca4dcb9c7fe13cdabb6f67fed12d042aa3
APCS2nd <APCS2nd@MHS-Dreyer-15>
2018-11-26 09:48:50 -0800

    Checkpt 2 done!

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -5,7 +5,8 @@ public class FracCalc {
 
     public static void main(String[] args) 
     {
-    	parts("3_1/2");
+    	//parts("2");
+    	//names(parts("3_1/2"));
         // TODO: Read the input from the user and call produceAnswer with an equation
     	System.out.println("What is the equation you'd like me to compute?");
     	Scanner userInput = new Scanner(System.in);
@@ -41,22 +42,41 @@ public class FracCalc {
 //       int num1 = Integer.parseInt(operand1);
  //      int num2 = Integer.parseInt(operand2);
       
-        return operand2;
+        return names(parts(operand2));
     }
 	    
-   public static String parts(String input) {
+   public static String[] parts(String input) {
     	if(input.indexOf("_")>0 && input.indexOf("/")>0) {
-    		String[] splitUnderscore = input.split("_");
-    		String[] splitSlash =  Arrays.toString(splitUnderscore).split("/");
-   		System.out.println(Arrays.toString(splitSlash));
-   		//does it know the elements? test--> at 0,1,2
-    	return Arrays.toString(splitSlash);
-    	}else { 
-    		
-    		return input;
-   
+    		String[] splitChars = input.split("_|\\/"); //splits _ and / ,, "|" makes it not split "_/" ,, "\\" separates them?
+   		//System.out.println(Arrays.toString(splitChars));
+    	return splitChars;
+    	}else if (input.indexOf("_")<0 && input.indexOf("/")>0) {
+    		String[] splitUnder = input.split("/");
+    	//	System.out.println(Arrays.toString(splitUnder));
+    		return splitUnder;
+    	}else if (input.indexOf("_")<0 && input.indexOf("/")<0){
+    		String[] noSplit = {input};
+    	//	System.out.println(Arrays.toString(noSplit));
+    		return noSplit;
+    	}else {
+    	String[] test = {"I","can't","code"};
+    	return test;
    }
     // TODO: Fill in the space below with any helper methods that you think you will need
 }
-
+   public static String names(String[] parts) {
+	   String name = "";
+	   if(parts.length == 3) {
+		   name = "whole:" + parts[0] + " numerator:" + parts[1] + " denominator:" + parts[2];
+	   } else if (parts.length == 2) {
+		   name = "whole:0" + " numerator:" + parts[0] + " denominator:" + parts[1];
+	   } else if (parts.length == 1) {
+		   name =  "whole:" + parts[0] + " numerator:0" + " denominator:1";
+	   }
+	  // System.out.println(name);
+	   return name;
+   
+   }
 }
+
+
